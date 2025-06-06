9be68b2b80ccac6a94510bc78e19e825e1f28bca
Anthony T <antonis301@gmail.com>
2025-06-07 00:38:28 +0300

    Various fixes (more replicas + proper label cleanup)

--- up.sh
+++ up.sh
@@ -10,5 +10,10 @@ docker config rm init_script
 docker config create init_script ./init-pleroma.sh
 docker config rm pleroma_config
 docker config create pleroma_config ./prod.secret.exs
+
+for node in $(docker node ls --filter role=worker --format '{{.ID}}'); do
+    docker node update --label-rm pleroma_nd "$node"
+done
 docker node update --label-add pleroma_nd=true $DB_NODE_NAME
+
 docker stack deploy -c docker-stack.yml $STACK_NAME
